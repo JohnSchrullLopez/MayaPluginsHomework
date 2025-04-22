0fd99104b40609db610d4cd5a3b227b542d69ebd
John Schrull Lopez <schrulll@student.uiwtx.edu>
2025-04-22 14:26:35 -0500

    Added add root joint function

--- src/MayaToUE.py
+++ src/MayaToUE.py
@@ -33,6 +33,21 @@ class MayaToUE:
         
         self.rootJnt = selection[0]
 
+    def AddRootJoint(self):
+        if not self.rootJnt:
+            raise Exception("No Root Joint Assigned, please set the root joint of your rig first")
+        
+        if mc.objExists(self.rootJnt):
+            currentRootPos = mc.xform(self.rootJnt, q=True, ws=True, t=True)
+            if currentRootPos[0] == 0 and currentRootPos[1] == 0 and currentRootPos[2] == 0:
+                raise Exception("Current root joint is at the origin already, no need to make a new one!")
+            
+        mc.select(cl=True)
+        rootJntName = self.rootJnt + "_root"
+        mc.joint(n=rootJntName)
+        mc.parent(self.rootJnt, rootJntName)
+        self.rootJnt = rootJntName
+
 class MayaToUEWidget(MayaWindow):
     def GetWidgetUniqueName(self):
         return "MayaToUEWidgetJRSL4172025407"
@@ -52,6 +67,15 @@ class MayaToUEWidget(MayaWindow):
         setSelectedAsRootJntBtn = QPushButton("Set Root Joint")
         setSelectedAsRootJntBtn.clicked.connect(self.SetSelectedAsRootJntBtnClicked)
         self.masterLayout.addWidget(setSelectedAsRootJntBtn)
+
+        addRootJntBtn = QPushButton("Add Root Joint")
+        addRootJntBtn.clicked.connect(self.AddRootJntBtnClicked)
+        self.masterLayout.addWidget(addRootJntBtn)
+
+    @TryAction
+    def AddRootJntBtnClicked(self):
+        self.mayaToUE.AddRootJoint()
+        self.rootJntText.setText(self.mayaToUE.rootJnt)
 
     @TryAction
     def SetSelectedAsRootJntBtnClicked(self):
