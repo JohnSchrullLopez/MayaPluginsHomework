31d4999206eaa9af51fc280f748c691fea4a6003
John Schrull Lopez <schrulll@student.uiwtx.edu>
2025-04-22 13:53:35 -0500

    added root joint se function

--- src/MayaToUE.py
+++ src/MayaToUE.py
@@ -1,5 +1,5 @@
 from MayaUtils import *
-from PySide2.QtWidgets import QVBoxLayout
+from PySide2.QtWidgets import QLineEdit, QPushButton, QVBoxLayout
 import maya.cmds as mc
 
 class AnimClip:
@@ -17,14 +17,35 @@ class MayaToUE:
         self.fileName = ""
         self.saveDir = ""
 
+    def SetSelectedJointAsRoot(self):
+        selection = mc.ls(sl=True, type="joint")
+        if not selection:
+            raise Exception("Wrong Selection! Please Select the root joint of your rig!")
+        
+        self.rootJnt = selection[0]
+
 class MayaToUEWidget(MayaWindow):
     def GetWidgetUniqueName(self):
         return "MayaToUEWidgetJRSL4172025407"
     
     def __init__(self):
         super().__init__()
+        self.mayaToUE = MayaToUE()
+
         self.setWindowTitle("Maya To UE")
         self.masterLayout = QVBoxLayout()
         self.setLayout(self.masterLayout)
+
+        self.rootJntText = QLineEdit()
+        self.rootJntText.setEnabled(False)
+        self.masterLayout.addWidget(self.rootJntText)
+
+        setSelectedAsRootJntBtn = QPushButton("Set Root Joint")
+        setSelectedAsRootJntBtn.clicked.connect(self.SetSelectedAsRootJntBtnClicked)
+        self.masterLayout.addWidget(setSelectedAsRootJntBtn)
+
+    def SetSelectedAsRootJntBtnClicked(self):
+        self.mayaToUE.SetSelectedJointAsRoot()
+        self.rootJntText.setText(self.mayaToUE.rootJnt)
 
 MayaToUEWidget().show()
