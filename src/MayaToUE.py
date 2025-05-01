c6a5b7625c66606c5522c41b9566432f9a0ac144
John Schrull Lopez <schrulll@student.uiwtx.edu>
2025-05-01 15:00:00 -0500

    Added call to UE import function in MayaToUE plugin

--- src/MayaToUE.py
+++ src/MayaToUE.py
@@ -82,6 +82,12 @@ class MayaToUE:
         command = "".join(commandLines)
         print(commandLines)
 
+        remoteExec = remote_execution.RemoteExecution()
+        remoteExec.start()
+        remoteExec.open_command_connection(remoteExec.remote_nodes)
+        remoteExec.run_command(command)
+        remoteExec.stop()
+
     def GetSkeletalMeshSavePath(self):
         savePath = os.path.join(self.saveDir, self.fileName + ".fbx")
         return os.path.normpath(savePath)
